1852cfc7708cfaddf7a948da254bf982aa28df79
Barisua-Nsaanee <bnsaanee7@gmail.com>
2022-06-03 10:11:51 -0700

    Numbers separated by commas then space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - a program that prints letters amd numbers using putchar
+ *
+ * Return: return 0 
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+
+		if (n == 9)
+		{
+		}
+		else
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
