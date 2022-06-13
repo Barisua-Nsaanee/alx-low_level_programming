de83a6cd44e82ba1554ee1df4daba10032b26bd6
Barisua-Nsaanee <bnsaanee7@gmail.com>
2022-06-13 05:44:18 -0700

    second

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * swap_int - main function
- * @a: pointer with the address value
- * @b: pointer with the address value
- * Description: this function takes a pointer to an int and updates the value it points to 98
+ * swap_int - main function.
+ * @a: Pointer with the address value.
+ * @b: Pointer with the address value.
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
  *
+ * Return: none.
  */
 void swap_int(int *a, int *b)
 {
 	int x;
 
 	x = *a;
-	*a= *b;
+	*a = *b;
 	*b = x;
-
 }
